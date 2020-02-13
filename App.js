dec9c561485cef0e85dd406f0d6924a98c920bf4
Hector Sanchez <hgsm@protonmail.com>
2020-02-13 11:41:27 -0400

    Using flex for profile

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Dimensions, Animated } from 'react-native';
 import { Images, Profiles } from './App/Themes';
+
+const SCREEN_HEIGHT = Dimensions.get('window').height
+const SCREEN_WIDTH = Dimensions.get('window').width
 
 export default class App extends React.Component {
   constructor() {
@@ -13,29 +16,34 @@ export default class App extends React.Component {
       age: haroldProfile.age,
       occupation: haroldProfile.occupation
     };
-   
-
   }
 
   render() {
     return (
       <View style={styles.container}>
 
-        <View style={styles.profiles}>
-          <Image source={this.state.profileImage} />
+        <View style={styles.header}>
 
-          <Text style={styles.name}>
-            {this.state.name}, 65
-          </Text>
+        </View>
+
+        <View style={styles.profiles}>
+          <Animated.View style = {styles.animatedView}>
+
+            <Image source={this.state.profileImage} style={styles.images}/>
+
+            <Text style={styles.name}>
+              {this.state.name}, 65
+            </Text>
+
+          </Animated.View>
 
           <Text style={styles.description}>GOD HIMSELF</Text>
+
         </View>  
 
-        <TouchableOpacity style={styles.touchable}>
+        <View style={styles.buttonsSect}>
 
-        <Image source={Images.rewind} style={styles.imageButtons}/>
-
-        </TouchableOpacity>
+        </View>
 
       </View>
     );
@@ -45,49 +53,38 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'gray',
+  },
+  header:{
+    height:60,
   },
   profiles:{
-       
-    borderRadius:10,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-    borderColor:'black',
+    flex:1,
+  },
+  animatedView:{
+    height:SCREEN_HEIGHT-200,
+    width:SCREEN_WIDTH,
+    padding:10,
+  },
+
+  images:{
+    flex:1,
+    height:null,
+    width:null,
+    resizeMode:'cover',
+    padding:10,
+    borderRadius:20,
+  },
+  buttonsSect:{
+    height:60,
   },
   name:{
     textAlign:'left',
     fontWeight:'bold',
   },
+
   description:{
     fontStyle:'italic',
     fontSize:11,
   },
-  touchable:{
-    borderWidth:1,
-    borderColor:'rgba(0,0,0,0.2)',
-    alignItems:'center',
-    width:100,
-    height:100,
-    backgroundColor:"white",
-    borderRadius:50,
-  },
-  buttons:{
-    backgroundColor:'white',
-    width:60,
-    height:60,
-    borderRadius:60/2,
-  },
-  imageButtons:{
-    flex:1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width:50,
-    height:50,
-  },
+
 });
